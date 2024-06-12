29f076b454b996373cea27ecfa077469ec170ebc
Максим Алекс <maximax9055yt@gmail.com>
2024-06-12 19:26:29 +0300

    Create user.js

---
+++ res/js/user.js
@@ -0,0 +1,5 @@
+let id = tg.initDataUnsafe.user.id;
+let username = "@" tg.initDataUnsafe.user.username;
+let name =  tg.initDataUnsafe.user.first_name;
+let familia =  tg.initDataUnsafe.user.last_name;
+document.write (`<h3>id: </h3><p>${id}</p><h3>username:</h3><p>${username}</p><h3>Имя:</h3><p>${name}</p>`);
